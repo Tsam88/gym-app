6d0c6ee46306a58d59bb3e4d8b24c99cd1d77539
tsam <tsiamouras.christos@gmail.com>
2022-09-20 01:06:24 +0300

    admin calendar - fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,9 +13,9 @@ import VueRouter from 'vue-router';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../css/adminApp.css'
 import '../css/app.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
