313da6efe72f8188b8e1fd276cf0a734cc3b4231
tsam <tsiamouras.christos@gmail.com>
2022-10-10 21:48:30 +0300

    fix favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
     <head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+{{--        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">--}}
 {{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
 {{--        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">--}}
 {{--        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=12, user-scalable=no">--}}
